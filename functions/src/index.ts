02022ca655fea70ee3a516bf85b43b7ed39a3c56
Niels Kersic <apps@niels.codes>
2020-07-01 01:50:31 +0200

    Created callable Cloud Function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,35 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const axios = require('axios');
+const qs = require('qs');
+
+export const spotifyLogin = functions.https.onCall(async (data, context) =>{
+
+  const code = data.code;
+  const endpoint = 'https://accounts.spotify.com/api/token';
+  const redirectURL = 'http://localhost:4200/callback';
+
+  const clientID = functions.config().spotify.id;
+  const clientSecret = functions.config().spotify.secret;
+
+  const requestData = {
+    'grant_type': 'authorization_code',
+    'code': code,
+    'redirect_uri': redirectURL,
+    'client_id': clientID,
+    'client_secret': clientSecret
+  };
+
+  const requestBody = qs.stringify(requestData);
+
+  try {
+
+    const res = await axios.post(endpoint, requestBody);
+    return res;
+
+  } catch (error) {
+    return error
+  }
+
+});
+
