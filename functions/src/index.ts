3581abe04c6269d6ecba81ac91f260ead67afa07
Niels Kersic <apps@niels.codes>
2020-07-01 16:07:31 +0200

    Save data to Firestore
    Save tokens to Firestore and count total saves

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,64 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 const axios = require('axios');
 const qs = require('qs');
 
 export const spotifyLogin = functions.https.onCall(async (data, context) =>{
 
+  // Get authorization code from function params
   const code = data.code;
+
+  // API setup
   const endpoint = 'https://accounts.spotify.com/api/token';
   const redirectURL = 'http://localhost:4200/callback';
 
+  // Get credentials from environment variables
   const clientID = functions.config().spotify.id;
   const clientSecret = functions.config().spotify.secret;
 
-  const requestData = {
+  const increment = admin.firestore.FieldValue.increment(1);
+
+  // Required request data
+  // TODO: Pass client data as Header with Base64 encoding
+  const requestBody = qs.stringify({
     'grant_type': 'authorization_code',
     'code': code,
     'redirect_uri': redirectURL,
     'client_id': clientID,
     'client_secret': clientSecret
-  };
+  });
 
-  const requestBody = qs.stringify(requestData);
-
+  // Try calling the Spotify API
   try {
 
-    const res = await axios.post(endpoint, requestBody);
-    console.log(res.data);
-    return 'success';
+    const tokenRes = await axios.post(endpoint, requestBody);
+
+    // Get returned data
+    const tokenData = {
+      token: tokenRes.data.access_token,
+      expiresIn: parseInt(tokenRes.data.expires_in),
+      refreshToken: tokenRes.data.refresh_token,
+      timestamp: admin.firestore.Timestamp.now()
+    };
+
+    // Create references to the --stats-- document and a new odcument
+    const statsRef = admin.firestore().collection('presaves').doc('--stats--');
+    const docRef = admin.firestore().collection('presaves').doc();
+
+    const batch = admin.firestore().batch();
+    batch.set(docRef, tokenData);
+    batch.set(statsRef, { saves: increment }, { merge: true });
+    await batch.commit();
+
+    // const userRes
+
+    return 'foobar';
 
   } catch (error) {
-    return 'no success'
+    return 'error'
   }
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,16 +17,13 @@ export class HomeComponent implements OnInit {
     const rootUrl = 'https://accounts.spotify.com/authorize';
     const clientID = 'e927df0934d7411181641fbd99a56f3c';
     const redirectURL = 'http://localhost:4200/callback';
-    const scope = 'user-library-modify';
+    const scope = 'user-library-modify user-read-private';
     const state = 'foobar';
 
-    const loginUrl = `${rootUrl}?client_id=${clientID}&response_type=code&redirect_uri=${redirectURL}&scope=${scope}&state=${state}&show_dialog=true`;
+    const loginUrl = `${rootUrl}?client_id=${clientID}&response_type=code&redirect_uri=${redirectURL}&scope=${encodeURIComponent(scope)}&state=${state}&show_dialog=true`;
 
     window.location.href = loginUrl;
 
   }
 
 }
-
-
-// http://localhost:4200/callback?code=AQDq98jvGzq_PiqJf0vGMfQopdGyf-k52sTRarRaysNhZMv3RDlRuY7jDCOnLaL-K91ZiSn1k76MDSvOccb64pBY60AtDljZ9CKKlbBfOw3LzF0KXzeFLFXnVhxYoY0zazeHxpRPV8dgLAqKgQN5Lo7J4mIIHfNZisAAhHgfgAg8UIa01xz2gSFLrtOG8lDExdGeVeIlsA&state=foobar
