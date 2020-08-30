e508d89193d10802da893fc9e009cb5cd2992a0d
Niels Kersic <apps@niels.codes>
2020-08-30 14:22:58 +0200

    Configured development deployment
    Configured second Firebase project as development environment. Added NPM deploy scripts for both environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDCF3Bl0KvlDjjsnK5i6TLa9NZjCetgBPE',
-    authDomain: 'presave-app.firebaseapp.com',
-    databaseURL: 'https://presave-app.firebaseio.com',
-    projectId: 'presave-app',
-    storageBucket: 'presave-app.appspot.com',
-    messagingSenderId: '565477002562',
-    appId: '1:565477002562:web:6bb7de375ed1a9e1438cdb',
-    measurementId: 'G-5WJ2N84FVF'
+    apiKey: 'AIzaSyBMr0tMR-32Ml53VDIhwzzdYtCHnxfospk',
+    authDomain: 'presave-app-dev.firebaseapp.com',
+    databaseURL: 'https://presave-app-dev.firebaseio.com',
+    projectId: 'presave-app-dev',
+    storageBucket: 'presave-app-dev.appspot.com',
+    messagingSenderId: '142585143182',
+    appId: '1:142585143182:web:869829d752cdc4b525cedf',
+    measurementId: 'G-60G7PF9HYR'
   }
 };
 
