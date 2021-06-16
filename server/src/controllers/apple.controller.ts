81c6a20e32058290332d14b3a19e92a4a30327ef
Niels Kersic <hi@niels.codes>
2021-06-16 19:51:49 +0200

    Add error handling for Dev Token endpoint

--- server/src/controllers/apple.controller.ts
+++ server/src/controllers/apple.controller.ts
@@ -1,7 +1,18 @@
 import {Request, Response} from 'express';
 import {createAppleToken} from '../services/apple.services';
 
+/** Controller to handle Apple Dev Token requests */
 export const appleTokenHandler = async (req: Request, res: Response) => {
-  const token = createAppleToken();
-  res.send(token);
+  try {
+    const token = createAppleToken();
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
