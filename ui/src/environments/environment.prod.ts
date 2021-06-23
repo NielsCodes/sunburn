017dae02c764d5bf3b438c615fac35ff931200ca
Niels Kersic <hi@niels.codes>
2021-06-23 21:37:48 +0200

    Update prod API endpoint

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   /** API endpoint for authentication purposes */
-  endpoint: 'https://sunburn.apis.niels.codes',
+  // endpoint: 'https://sunburn.apis.niels.codes',
+  endpoint: 'https://sunburn-api-prod-uhv4eba3aa-uc.a.run.app',
   /** Redirect URL for Spotify authentication */
   redirect: 'https://sunburn.niels.codes/callback'
 };
